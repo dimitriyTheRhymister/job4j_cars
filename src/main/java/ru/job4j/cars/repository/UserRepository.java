8bc9fb703234a6ffebc216800991db88f5eb17c9
dimitriy <rhymister19@gmail.com>
2025-11-17 20:17:54 +0300

    2. HQL [#6874 #555002]_2

--- src/main/java/ru/job4j/cars/repository/UserRepository.java
+++ src/main/java/ru/job4j/cars/repository/UserRepository.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 import ru.job4j.cars.model.User;
 
 import java.util.List;
@@ -54,8 +55,13 @@ public class UserRepository {
      */
     public void update(User user) {
         execute(session -> {
-            session.update(user);
-            return null;
+            Query query = session.createQuery(
+                    "UPDATE User SET login = :login, password = :password WHERE id = :id"
+            );
+            query.setParameter("login", user.getLogin());
+            query.setParameter("password", user.getPassword());
+            query.setParameter("id", user.getId());
+            return query.executeUpdate();  // HQL DML подход
         });
     }
 
@@ -65,10 +71,9 @@ public class UserRepository {
      */
     public void delete(Integer userId) {
         execute(session -> {
-            User user = new User();
-            user.setId(userId);
-            session.delete(user);
-            return null;
+            Query query = session.createQuery("DELETE User WHERE id = :id");
+            query.setParameter("id", userId);
+            return query.executeUpdate();  // HQL DML подход
         });
     }
 
@@ -77,10 +82,12 @@ public class UserRepository {
      * @return список пользователей.
      */
     public List<User> findAllOrderById() {
-        return execute(session ->
-                session.createQuery("FROM User ORDER BY id", User.class)
-                        .list()
-        );
+        return execute(session -> {
+            Query<User> query = session.createQuery(
+                    "FROM User ORDER BY id", User.class
+            );
+            return query.list();  // HQL подход
+        });
     }
 
     /**
@@ -88,9 +95,13 @@ public class UserRepository {
      * @return пользователь.
      */
     public Optional<User> findById(Integer userId) {
-        return execute(session ->
-                Optional.ofNullable(session.get(User.class, userId))
-        );
+        return execute(session -> {
+            Query<User> query = session.createQuery(
+                    "FROM User WHERE id = :id", User.class
+            );
+            query.setParameter("id", userId);
+            return query.uniqueResultOptional();  // HQL подход
+        });
     }
 
     /**
@@ -99,11 +110,13 @@ public class UserRepository {
      * @return список пользователей.
      */
     public List<User> findByLikeLogin(String key) {
-        return execute(session ->
-                session.createQuery("FROM User WHERE login LIKE :login", User.class)
-                        .setParameter("login", "%" + key + "%")
-                        .list()
-        );
+        return execute(session -> {
+            Query<User> query = session.createQuery(
+                    "FROM User WHERE login LIKE :login", User.class
+            );
+            query.setParameter("login", "%" + key + "%");
+            return query.list();  // HQL подход
+        });
     }
 
     /**
@@ -112,10 +125,12 @@ public class UserRepository {
      * @return Optional or user.
      */
     public Optional<User> findByLogin(String login) {
-        return execute(session ->
-                session.createQuery("FROM User WHERE login = :login", User.class)
-                        .setParameter("login", login)
-                        .uniqueResultOptional()
-        );
+        return execute(session -> {
+            Query<User> query = session.createQuery(
+                    "FROM User WHERE login = :login", User.class
+            );
+            query.setParameter("login", login);
+            return query.uniqueResultOptional();  // HQL подход
+        });
     }
 }
